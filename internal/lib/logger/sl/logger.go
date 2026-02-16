d0fff3188d1f1c9ae6dc5ba5fc73e92d4d841931
Yaoguao <glenioJ@yandex.ru>
2026-02-16 14:49:38 +0400

    add logger

---
+++ internal/lib/logger/sl/logger.go
@@ -0,0 +1,39 @@
+package sl
+
+import (
+	"io"
+	"log/slog"
+)
+
+var logger *slog.Logger
+
+func InitLogger(env string, w io.Writer) *slog.Logger {
+	switch env {
+	case "dev":
+		logger = SetupPrettySlog(w, slog.LevelDebug)
+	case "local":
+		logger = SetupPrettySlog(w, slog.LevelDebug)
+	default:
+		logger = SetupPrettySlog(w, slog.LevelInfo)
+	}
+
+	return logger
+}
+
+func Err(err error) slog.Attr {
+	return slog.Attr{
+		Key:   "error",
+		Value: slog.StringValue(err.Error()),
+	}
+}
+
+func SetupPrettySlog(w io.Writer, level slog.Level) *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: level,
+		},
+	}
+	handler := opts.NewPrettyHandler(w)
+
+	return slog.New(handler)
+}
