ed13483584f9d71bd0552954fdd39296988fd502
Yaoguao <glenioJ@yandex.ru>
2026-02-18 15:30:37 +0400

    change errors

--- internal/http-server/handlers/errors.go
+++ internal/http-server/handlers/errors.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 	"wallet-service/pkg/helpers"
 )
@@ -17,25 +16,4 @@ func ErrorResponse(w http.ResponseWriter, r *http.Request, status int, message a
 
 func BadRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	ErrorResponse(w, r, http.StatusBadRequest, err.Error())
-}
-
-func ServerErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
-
-	message := "the server encountered a problem and could not process your request"
-
-	ErrorResponse(w, r, http.StatusInternalServerError, message)
-}
-
-func NotFoundResponse(w http.ResponseWriter, r *http.Request) {
-	message := "the requested resource could not be found"
-	ErrorResponse(w, r, http.StatusNotFound, message)
-}
-
-func MethodNotAllowResponse(w http.ResponseWriter, r *http.Request) {
-	message := fmt.Sprintf("the %s method is not supported for this response", r.Method)
-	ErrorResponse(w, r, http.StatusMethodNotAllowed, message)
-}
-
-func FailedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
-	ErrorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
